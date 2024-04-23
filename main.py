8973f8c02d1b105c4c62f85d9a4e725c6d51ce0b
mkacprzak5 <167193709+mkacprzak5@users.noreply.github.com>
2024-04-23 15:17:58 +0200

    feat: remove was added

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from mode.data import users
-from utils.crud import read, add_user
+from utils.crud import read, add_user, search, remove_user
 
 if __name__ == '__main__':
     print(f'witaj {users[0]["name"]}')
 
-    read(users)
-    add_user(users)
-    read(users)
+    while True:
+        print('0. zakończ program ')
+        print('1. wyświetl znajomych ')
+        print('2. dodaj znajomego')
+        print('3. szukaj znajomego')
+        print('4. usuń znajomego')
+        menu_option = input('wybierz opcję menu: ')
+        if menu_option == '0': break
+        if menu_option == '1': read(users)
+        if menu_option == '2': add_user(users)
+        if menu_option == '3': search(users)
+        if menu_option == '3': remove_user(users)
+
--- utils/crud.py
+++ utils/crud.py
@@ -1,9 +1,25 @@
-def read(users: list[dict[str, str]])->None:
+def read(users: list[dict[str, str]]) -> None:
     for user in users[1:]:
         print(f'twój znajomy {user["name"]} opublikował {user["post"]} posty ')
+
 
 def add_user(lista: list) -> None:
     user_name = input('Podaj imię: ')
     user_surname = input('Podaj nazwisko: ')
     user_post = input('Podaj ile postów: ')
     lista.append({"name": user_name, "surname": user_surname, "post": user_post})
+
+
+def search(users: list[dict[str, str]]) -> None:
+    user_name = input('Kogo szukasz?: ')
+    for user in users[1:]:
+        if user["name"] == user_name:
+            print(f'znaleziono użytkownika {user}')
+
+
+def remove_user(users: list[dict[str, str]]) -> None:
+    user_name = input('Kogo usunąć?: ')
+    for user in users[1:]:
+        if user["name"] == user_name:
+            print(f'znaleziono użytkownika {user['name']}')
+            users.remove(user)
