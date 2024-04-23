621977f7b6da6f3802fa901ce3ca215d0fa70fc4
mkacprzak5 <167193709+mkacprzak5@users.noreply.github.com>
2024-04-23 14:26:06 +0200

    feat: add create was added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from mode.data import users
-from utils.crud import read
+from utils.crud import read, add_user
 
 if __name__ == '__main__':
     print(f'witaj {users[0]["name"]}')
 
-read(users)
+    read(users)
+    add_user(users)
+    read(users)
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,9 @@
 def read(users: list[dict[str, str]])->None:
     for user in users[1:]:
         print(f'twój znajomy {user["name"]} opublikował {user["post"]} posty ')
+
+def add_user(lista: list) -> None:
+    user_name = input('Podaj imię: ')
+    user_surname = input('Podaj nazwisko: ')
+    user_post = input('Podaj ile postów: ')
+    lista.append({"name": user_name, "surname": user_surname, "post": user_post})
